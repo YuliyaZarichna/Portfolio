45bbc6cf3f1ea935d797da41894a483c7338abc3
yuliyazarichna <zarichna19@gmail.com>
2020-03-31 21:20:47 +0200

    tutorial lessons

---
+++ .next/static/-gMGmdWZttu-mPOIF8qaL/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = (function(a){return {"/":[a],"/index":[a],"/p/[id]":[a]}}("static\u002Fchunks\u002Fef5082e1260cd17ca6c2fc03766bd3fc02f9f163.6c44f0048d1351987336.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
---
+++ pages/about.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Layout from '../components/MyLayout';
+
+
+const about = () => (
+    <Layout>
+        <p>This is the about page</p>
+    </Layout>
+)
+
+export default about;
