8d00f3435704d111a98ed2e5baee2419d0c6a8f0
yuliyazarichna <zarichna19@gmail.com>
2020-05-06 14:37:30 +0200

    changed nav active color

--- components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
+++ components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -1,11 +1,28 @@
 import classes from './NavigationItem.module.css'
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 
 const navigationItem = (props) => {
+    const router = useRouter()
+    const style = {
+        marginRight: 10,
+        color: router.pathname === props.link ? 'red' : 'black',
+    }
+    const handleClick = e => {
+        e.preventDefault()
+        router.push(props.link)
+    }
+
     return (
         <Link href={props.link} >
-            <div className={classes.NavigationItem}>
-                <a className={props.active ? classes.active : null}>{props.children}</a>
+            <div>
+                <a
+                    onClick={handleClick}
+                    className={classes.NavigationItem}
+                    style={style}
+                >
+                    {props.children}
+                </a>
             </div>
         </Link>
     )
